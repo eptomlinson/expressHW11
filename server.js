f9b7d97d518dffa90db0b13fb467fd38bf7c98d8
Erik Tomlinson <eptomlinson@gmail.com>
2020-10-26 09:46:06 -0500

    finalized

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+// const studentList = require("./db/studentList");
+// console.log(studentList);
+// const apiRoutes = require("./routes/apiRoutes");
+// const htmlRoutes = require("./routes/htmlRoutes");
+
+// Initialize the app and create a port
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Set up body parsing, static, and route middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+// app.use("/api", apiRoutes);
+// app.use("/", htmlRoutes);
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+});
+
+app.get("/api/notes", function (req, res) {
+    fs.readFile("./db/db.json", "utf8", function (err, data) {
+        if (err) throw err;
+        console.log(data);
+        const newData = JSON.parse(data);
+        res.json(newData);
+    })
+});
+
+app.post("/api/notes", function (req, res) {
+
+    // console.log(req.body);
+    fs.readFile("./db/db.json", "utf8", function (err, data) {
+        if (err) throw err;
+        console.log(data);
+        const noteList = JSON.parse(data);
+        noteList.push(req.body);
+        console.log(noteList);
+        fs.writeFile("./db/db.json", JSON.stringify(noteList), function (err) {
+            if (err) throw err;
+            console.log("added");
+            res.redirect("/notes");
+        });
+    });
+})
+
+app.delete("/api/notes/:id", function (req, res) {
+//splice or slice
+    res.json(req.params.name);
+})
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
